4c4bdaccae99f72fe9db88a8ffb519a930753f80
sbpipb <julianmacmang@gmail.com>
2024-09-09 21:33:17 +0800

    fix main cli

--- main.rb
+++ main.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/env ruby
 require 'byebug'
 require 'json'
+require_relative './query'
 
 def main
   file = File.read('clients.json')
   clients = JSON.parse(file)
-  Query.new(clients: clients).by_name
+  Query.new(clients:).by_name
 end
 
-#  Search through all clients and return those with names partially matching a given search query 
-def search_name(keyword = 'john', attribute = 'full_name')
-  # json = main
-  # puts json.select { |x| x[attribute].downcase.include? keyword }
+#  Search through all clients and return those with names partially matching a given search query
+def search_name(keyword)
+  puts Query.new.by_name(keyword:)
 end
 
 def search_email
-  # Find out if there are any clients with the same email in the dataset, and show those duplicates if any are found. 
+  # Find out if there are any clients with the same email in the dataset, and show those duplicates if any are found.
 end
 
+if ARGV.empty?
+  puts 'Usage: ruby main.rb <keyword>'
+  exit
+end
 
-main if __FILE__ == $PROGRAM_NAME
+search_name(ARGV[0])
+
+# search_name if __FILE__ == $PROGRAM_NAME
