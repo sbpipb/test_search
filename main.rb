0edae485e2fa3628037549d55f6d0cf1461dea7c
sbpipb <julianmacmang@gmail.com>
2024-09-09 21:39:36 +0800

    update cli commands and fix for search email and search name

--- main.rb
+++ main.rb
@@ -3,26 +3,22 @@ require 'byebug'
 require 'json'
 require_relative './query'
 
-def main
-  file = File.read('clients.json')
-  clients = JSON.parse(file)
-  Query.new(clients:).by_name
-end
-
 #  Search through all clients and return those with names partially matching a given search query
 def search_name(keyword)
-  puts Query.new.by_name(keyword:)
+  Query.new.by_name(keyword:)
 end
 
 def search_email
+  Query.new.by_email
   # Find out if there are any clients with the same email in the dataset, and show those duplicates if any are found.
 end
 
-if ARGV.empty?
-  puts 'Usage: ruby main.rb <keyword>'
+if ARGV[0] == 'name' && ARGV[1]
+  puts search_name(ARGV[1]) 
+elsif ARGV[0] == 'email'
+  puts search_email
+else
+  puts 'Usage: ruby main.rb name <keyword> # to search for name based on keyword'
+  puts 'Usage: ruby main.rb email # to search for records with duplicate emails'
   exit
 end
-
-search_name(ARGV[0])
-
-# search_name if __FILE__ == $PROGRAM_NAME
