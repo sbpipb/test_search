b877ea109a60a7c0f4f6ab463ab2796dafab1ab5
sbpipb <julianmacmang@gmail.com>
2024-09-09 20:54:14 +0800

    add tests and query update

--- query.rb
+++ query.rb
@@ -1,17 +1,17 @@
 require 'json'
 
 class Query
-  attr_reader :keyword, :clients, :attribute
+  attr_reader :keyword, :data, :attribute
 
   def initialize(keyword:, file: 'clients.json')
     @keyword = keyword.downcase
     file = File.read(file)
-    @clients = JSON.parse(file, symbolize_names: true)
+    @data = JSON.parse(file, symbolize_names: true)
   end
-
+  
   def by_name
     @attribute = :full_name
-    @clients.select { |x| x[attribute].downcase.include? keyword }
+    @data.select { |x| x[attribute].downcase.include? keyword }
   end
 
   def by_email; end
--- test.rb
+++ test.rb
@@ -3,44 +3,46 @@ require 'byebug'
 require_relative './query'
 
 describe Query do
-  context 'with a keyword that has two matches' do
-    it 'searches for a match in full_name attribute' do
-      query = Query.new(keyword: 'Jane', file: 'clients.json')
-      expect(query.by_name).to match_array(
-        [{
-          id: 2,
-          full_name: 'Jane Smith',
-          email: 'jane.smith@yahoo.com'
-        },
-         {
-           id: 15,
-           full_name: 'Another Jane Smith', email: 'jane.smith@yahoo.com'
-         }]
-      )
+  describe '#by_name' do
+    context 'with a keyword that has two matches' do
+      it 'searches for a match in full_name attribute' do
+        query = Query.new(keyword: 'Jane', file: 'clients.json')
+        expect(query.by_name).to match_array(
+          [{
+            id: 2,
+            full_name: 'Jane Smith',
+            email: 'jane.smith@yahoo.com'
+          },
+           {
+             id: 15,
+             full_name: 'Another Jane Smith', email: 'jane.smith@yahoo.com'
+           }]
+        )
+      end
     end
-  end
 
-  context 'with a record that has a single match' do
-    it 'searches by name' do
-      query = Query.new(keyword: 'Emily', file: 'clients.json')
-      expect(query.by_name).to match_array(
-        [
-          {
-            "id": 5,
-            "full_name": 'Emily Brown',
-            "email": 'emily.brown@aol.com'
-          }
-        ]
-      )
+    context 'with a record that has a single match' do
+      it 'searches by name' do
+        query = Query.new(keyword: 'Emily', file: 'clients.json')
+        expect(query.by_name).to match_array(
+          [
+            {
+              "id": 5,
+              "full_name": 'Emily Brown',
+              "email": 'emily.brown@aol.com'
+            }
+          ]
+        )
+      end
     end
-  end
 
-  context 'with a record with no match' do
-    it 'returns empty' do
-      query = Query.new(keyword: 'Jolianism', file: 'clients.json')
-      expect(query.by_name).to match_array(
-        []
-      )
+    context 'with a record with no match' do
+      it 'returns empty' do
+        query = Query.new(keyword: 'Jolianism', file: 'clients.json')
+        expect(query.by_name).to match_array(
+          []
+        )
+      end
     end
   end
 end
